4931a575f3e95b7e7358cec7a25750b4f6802298
Larisa Varga <larisa.vrg@yaho.com>
2024-02-21 15:49:05 +0200

    add header and make it work on mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(checkbox) {
+    const parent = checkbox.parentElement.parentElement;
+    if (checkbox.checked) {
+        parent.classList.add('open'); 
+    } else {
+        parent.classList.remove('open'); 
+    }
+}
